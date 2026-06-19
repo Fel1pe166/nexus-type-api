b30962d511b01fe0451e4e09e924cf814214409d
Fel1pe166 <felipesatinho06@gmail.com>
2026-06-19 05:33:19 -0300

    Add initial Discord client setup

---
+++ src/discord.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences
+  ]
+});
+
+module.exports = { client };
